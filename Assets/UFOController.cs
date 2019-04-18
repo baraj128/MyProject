74ba0288b80df527bb504eee4f8aa1513f0b4002
america <baraj128@minidokaschools.org>
2019-04-18 11:07:08 -0600

    controls

--- Assets/UFOController.cs
+++ Assets/UFOController.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class UFOController : MonoBehaviour {
 
-	//Add Variables here
-
+    //Add Variables here
+    public Rigidbody2D theRB2d;
+    public float movementSpeed;
 
 
 	// Use this for initialization
 	void Start () {
-		
+        theRB2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		//Add Movement Code Here
-		
-	}
+		if(Input.GetKeyDown(KeyCode.W))
+        {
+            theRB2d.velocity += new Vector2(0f, 1.0f) * movementSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            theRB2d.velocity += new Vector2(0f, -1.0f) * movementSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            theRB2d.velocity += new Vector2(-1.0f, 0f  ) * movementSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            theRB2d.velocity += new Vector2(1.0f, 0.0f) * movementSpeed;
+        }
+    }
 }
