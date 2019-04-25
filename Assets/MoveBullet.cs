abeab2bc899009db36947fafcf325d40c3e8cd20
Alex <lopez914@minidokaschools.org>
2019-04-25 11:11:44 -0600

    UPD

---
+++ Assets/MoveBullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBullet : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public Rigidbody2D theRB2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB2D.velocity = transform.right * movementSpeed;
+
+    }
+
+
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+
+    }
+    private void OnTriggerEnter(Collider2D other)
+    {
+        if (other.gameObject.tag != "Player")
+        {
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/ShootWeapons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWeapons : MonoBehaviour
+{
+    public GameObject projectile;
+    public GameObject barrel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(projectile, barrel.transform.position, barrel.transform.rotation);
+        }
+    }
+}
