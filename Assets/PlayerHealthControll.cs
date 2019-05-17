b426834778d1c7849f5b6a4207ccb45b21a5b569
Alex <lopez914@minidokaschools.org>
2019-05-17 11:13:07 -0600

    upd

--- Assets/PlayerHealthControll.cs
+++ Assets/PlayerHealthControll.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealthControll : MonoBehaviour
 {
     public int health;
+    public string levelToLoad;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,8 @@ public class PlayerHealthControll : MonoBehaviour
     {
         if (health <= 0)
         {
-            gameObject.SetActive(false);
+            // gameObject.SetActive(false);
+            SceneManager.LoadScene(levelToLoad);
             //gameObject.SetActive(false);
         }
     }
